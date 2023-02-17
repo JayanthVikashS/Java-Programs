6ecb4e507dd5ee87d0817c15a5cb0bb47b626802
Jayanth Vikash S <jayanth082001@gmail.com>
2023-02-17 15:08:56 +0530

    Commit

---
+++ src/Evenodd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Evenodd{
+    public static void main(String[] args){
+       System.out.println("Enter the Number to be checked");
+       Scanner input = new Scanner(System.in);
+       int num1 = input.nextInt();
+       if (num1%2 == 0) {
+           System.out.println("The number is Even" + num1);
+       }
+       else{
+           System.out.println("The number is Odd" + num1);
+       }
+    }
+}
---
+++ src/Name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Name{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String Name = input.next();
+        System.out.println("Welcome to Java Learning " + Name);
+    }
+}
